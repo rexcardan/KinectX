b45b454fd7637812d0cf8b2bba45deb7abc0ab5d
Rex Cardan <rexcardan@gmail.com>
2018-10-09 14:01:26 -0500

    Improved service to start after 20 seconds

--- KxRemoteService/Service1.cs
+++ KxRemoteService/Service1.cs
@@ -1,11 +1,15 @@
 ﻿using NLog;
+using System;
 using System.ServiceProcess;
+using System.Threading;
+using System.Timers;
 
 namespace KxRemoteService
 {
     public partial class Service1 : ServiceBase
     {
         private ILogger _logger = LogManager.GetCurrentClassLogger();
+        private System.Timers.Timer timer;
 
         public Service1()
         {
@@ -14,11 +18,23 @@ namespace KxRemoteService
 
         protected override void OnStart(string[] args)
         {
+            // Set up a timer to trigger at next scheduled time
+            timer = new System.Timers.Timer();
+            //We are going to come back in 20 seconds to do something so the service doesn't hang
+            var nextTime = 20000; //20 seconds from now
+            timer.Interval = nextTime;
+            timer.AutoReset = false;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
             _logger.Info("Starting service...");
             KinectX.Network.KxServer.Start();
             _logger.Info("Service started. Waiting for stop...");
             _logger.Info("Service stopped");
-
+            timer.Stop();
         }
 
         protected override void OnStop()
